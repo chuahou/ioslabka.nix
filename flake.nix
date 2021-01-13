02da9ec0b2e6e78673930d23a769b647d5d7071b
Chua Hou <human+github@chuahou.dev>
2021-01-14 02:15:22 +0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Custom builds of Iosevka";
+
+  inputs = {
+    nixpkgs.url     = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        rec {
+          packages = {
+            iosevkaFixed = pkgs.iosevka.override { set = "fixed"; };
+            ioslabka     = pkgs.iosevka.override {
+              set = "fixed";
+              privateBuildPlan = {
+                family = "Ioslabka";
+                design = [
+                  "v-a-doublestorey"               # cv01
+                  "v-f-serified"                   # cv84
+                  "v-i-serified"                   # cv03
+                  "v-k-cursive"                    # cv70
+                  "v-l-serified"                   # cv07
+                  "v-r-serified"                   # cv86
+                  "v-t-flat-hook"                  # VXDC
+                  "v-u-with-bar"                   # cv89
+                  "v-y-cursive"                    # cv49
+                  "v-capital-g-toothless"          # cv92
+                  "v-capital-j-serifed-both-sides" # VXDA
+                  "v-capital-m-flat-bottom"        # VXCJ
+                  "v-zero-dotted"                  # cv14
+                  "v-one-base"                     # cv51
+                  "v-three-flattop"                # cv46
+                  "v-six-open-contour"             # VXAF
+                  "v-nine-open-contour"            # cv97
+                  "v-paren-larger-contour"         # VXAO
+                  "v-dollar-open"                  # cv38
+                  "sp-fixed"
+                  "slab"
+                ];
+              };
+          };
+        };
+        defaultPackage = packages.ioslabka;
+      });
+}
